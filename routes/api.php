9eb581d05ca030a59ccc774d7438174c30fcd906
dwipapap <dwipahyaki@gmail.com>
2026-02-16 20:50:14 +0700

    update API

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CourseController;
+use App\Http\Controllers\Api\MaterialController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -16,4 +17,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::put('/courses/{id}', [CourseController::class, 'update']);
     Route::delete('/courses/{id}', [CourseController::class, 'destroy']);
     Route::post('/courses/{id}/enroll', [CourseController::class, 'enroll']);
+
+    Route::post('/materials', [MaterialController::class, 'store']);
+    Route::get('/materials/{id}/download', [MaterialController::class, 'download']);
 });
